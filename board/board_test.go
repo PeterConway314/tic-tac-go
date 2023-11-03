d26e30d7e98713faf739788a9ada5c416eb2eb78
Peter Conway <peterconway3.14@gmail.com>
2023-11-03 09:36:26 +0000

    Write a stub test function for the board package

---
+++ board/board_test.go
@@ -0,0 +1,13 @@
+package board_test
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestBoard(t *testing.T) {
+	Convey("Given a board in some state", t, func() {
+		// ...
+	})
+}
