732cfcba318d305eab85964b335b253714167656
Peter Conway <peterconway3.14@gmail.com>
2023-11-06 09:45:17 +0000

    Add test case structure and cleanup to test stubs

--- board/board_test.go
+++ board/board_test.go
@@ -2,12 +2,44 @@ package board_test
 
 import (
 	"testing"
+	"tic-tac-go/board"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+var (
+	b board.Board
+)
+
+func withCleanup(f func()) func() {
+	return func() {
+		Reset(cleanup)
+		f()
+	}
+}
+
+func cleanup() {
+	b.InitialiseBoard()
+}
+
 func TestBoard(t *testing.T) {
-	Convey("Given a board in some state", t, func() {
-		// ...
-	})
+	Convey("Given a board in some state", t, withCleanup(func() {
+		type testCase struct {
+			description string
+		}
+		cases := []testCase{
+			{
+				description: "test case 1",
+			},
+			{
+				description: "test case 2",
+			},
+		}
+
+		for _, tc := range cases {
+			Convey(tc.description, func() {
+				So(true, ShouldBeTrue)
+			})
+		}
+	}))
 }
